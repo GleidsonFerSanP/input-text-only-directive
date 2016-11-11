9ef9b9e1f0983a310b2d885547afd46841f08ce3
Gleidson FerSanP <gleidson.ferreirasantos@gmail.com>
2016-11-11 12:15:36 -0200

    adjusted pattern for permit space

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # input-text-only-directive
 AngularJs based directive with input accepting only letters
+
+Usage Ex:
+
+
+Permit space in text
+<input type="text" input-text-only space="true">
+
+Not permit space in text
+<input type="text" input-text-only >
--- input-only-text.js
+++ input-only-text.js
@@ -1,41 +1,46 @@
 angular.module("input-text-mask", [])
 .directive('inputTextOnly', ['$timeout', 'TextMaskService', function($timeout, TextMaskService) {
-	return {
-		require: 'ngModel',
-		link: function(scope, element, attrs, ctrl) {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, ctrl) {
 
 
-			var execute = function () {
-				scope.$apply(function () {
+            var execute = function () {
+                scope.$apply(function () {
 
-					ctrl.$setViewValue(TextMaskService.fromStringToTextOnly(ctrl.$modelValue));
-					ctrl.$render();
-				});
-			};
-			element.bind('keyup', function (event) {
-				if(event.key !== ' ' && attrs.space)
-					execute();
-			});
+                    ctrl.$setViewValue(TextMaskService.fromStringToTextOnly(ctrl.$modelValue));
+                    ctrl.$render();
+                });
+            };
+            element.bind('keyup', function (event) {
 
-			$timeout(function () { 
-				execute();
-			}, 500);
-		}
-	};
+                if(event.key === ' ' && attrs.space){
+                    return;
+                }else{
+                    execute();
+                }
+
+            });
+
+            $timeout(function () {
+                execute();
+            }, 500);
+        }
+    };
 }])
 .factory("TextMaskService", [ "$filter", function ($filter) {
-	var _invalidsCharacteres = /[!?.;:,@#$%*=0-9\"\'\/.><\\|\(\)\]\[\{\}\-_+]+/;
+    var _invalidsCharacteres = /[!?.;:,&@#$%*=0-9\"\'\/.><\\|\(\)\]\[\{\}\-_+]+/;
 
-	var _fromStringToTextOnly = function (string) {
-		return _fromStringOnly(string);
-	};
+    var _fromStringToTextOnly = function (string) {
+        return _fromStringOnly(string);
+    };
 
-	var _fromStringOnly = function (string) {
-		if (!string) return '';
-		return string.replace(_invalidsCharacteres, '');
-	}
+    var _fromStringOnly = function (string) {
+        if (!string) return '';
+        return string.replace(_invalidsCharacteres, '');
+    }
 
-	return {
-		fromStringToTextOnly: _fromStringToTextOnly
-	};
+    return {
+        fromStringToTextOnly: _fromStringToTextOnly
+    };
 }]);
